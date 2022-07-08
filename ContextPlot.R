543ea933c2f263910332ae96638a6cfd925a7674
IgorUlitsky <igor.ulistky@weizmann.ac.il>
2022-07-08 11:59:30 +0300

    Updated ContextPlot

--- ContextPlot.R
+++ ContextPlot.R
@@ -11,6 +11,12 @@ rootDir = config["rootDir",1]
 File = config["finalTab",1]
 condition  = config["condition",1]
 numOfReplicates = config["numOfReplicates",1]
+outFile = config["outFile",1]
+
+#rootDir = "/home/labs/ulitsky/igoru/"
+#File = "twistGFP_rep3.processTwist.finalTab.txt"
+#condition="Cprime_"
+#numOfReplicates=3
 
 finalTab <- read.table(paste0(rootDir,File),sep="\t",stringsAsFactors=FALSE,quote="",comment="",header=T)
 
@@ -26,7 +32,7 @@ makePlot<-function(gene,subMat,windowSize,ylab,drawLegend=T,title=gene){
   segA$min <- apply(segA, 1, min, na.rm=T)
   segA$max <- apply(segA, 1, max, na.rm=T)
   segA$Index <- subMat[,"Index"]
-  segA <- segA[order(segA$Index),]
+  #segA <- segA[order(segA$Index),]
   
   minAll = min(c(ncA),na.rm=T)
   maxAll = max(c(ncA),na.rm=T)
@@ -35,17 +41,19 @@ makePlot<-function(gene,subMat,windowSize,ylab,drawLegend=T,title=gene){
   myOrder <- order(subMat$Index)[usable]
   #myCols = sapply(subMat$Repetitive,function(x){ if (x) {"black"} else {"darkgrey"}})[myOrder]
   
-  for (start in seq(1,length(ncA),windowSize)){
-    end = min(start+windowSize,length(ncA))
+  for (myStart in seq(1,length(ncA),windowSize)){
+    myEnd = min(myStart+windowSize,length(ncA))
     if (!is.na(minAll)){
       myTitle = title
-      if (start>1 || end <length(ncA)){
-        myTitle  = paste0(title," ",start,":",(end-1))
+      if (myStart>1 || myEnd <length(ncA)){
+        myTitle  = paste0(title," ",myStart,":",(myEnd-1))
       }
-      at = plot(start:end,ncA[myOrder[start:end]],main=myTitle,ylim=c(min(-1.5,minAll),max(1.5,maxAll)),col="red",pch=20,ylab=ylab,xlab="Tile number",bty="n")
-      lines(start:end,ncA[myOrder[start:end]],type="l",col="red")
-      for (i in 1:nrow(segA)){
-        segments(x0 = segA[i,"Index"], y0 = segA[i,"min"], y1 = segA[i,"max"], col = "lightgray")
+      plot(myStart:myEnd,ncA[myOrder[myStart:myEnd]],main=myTitle,ylim=c(min(-1.5,minAll),max(1.5,maxAll)),col="red",pch=20,ylab=ylab,xlab="Tile number",bty="n")
+      lines(myStart:myEnd,ncA[myOrder[myStart:myEnd]],type="l",col="red")
+      for (i in myStart:myEnd){
+        segments(x0 = i, 
+                 y0 = segA[myOrder[i],"min"], 
+                 y1 = segA[myOrder[i],"max"], col = "lightgray")
       }
       
       abline(h=0, col = "lightgray")
@@ -59,10 +67,10 @@ makePlot<-function(gene,subMat,windowSize,ylab,drawLegend=T,title=gene){
 
 
 ## generating context plots
-windowSize= 200
+windowSize= 100
 par(mar=c(4,4,4,4))
 par(mfrow=c(1,1))
-pdf("ContextPlot.pdf",width=12)
+pdf(outFile,width=12)
 for (gene in unique(finalTab$GeneName)){
   print(paste(gene))
   subMat<-finalTab[grepl(paste0("^",gene,"$"),finalTab$GeneName),]
